939b5c299d63240b5e2a4c03e67ef9aeccda6920
Chase Brooks <meovermymind@gmail.com>
2022-11-30 18:03:29 -0500

    js

---
+++ .vscode/CALCULATOR.md
@@ -0,0 +1,25 @@
+- Create a calculator container
+  with a dark grey background
+  add height
+  add width
+  rounded borders
+
+- Create number display container
+  create a div
+  add some hardcoded text
+  add rounded border
+  add background colors of light green
+  add hieght
+  add width
+
+- Create button container
+  add 4 rows
+  each row has 4 columns with a button with round edges
+  row 1 buttons - 7,8,9,division symbol
+  row 2 buttons - 4,5,6,x
+  row 3 buttons - 1,2,3,-
+  row 4 buttons - 0,.,+,=
+  give each button a backgroun color of white
+  give equal sign a background color orange and font color white
+
+- make table of buttons
--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
-console.log("is this working");
+const userAge = prompt("How old are you?");
 
-const myName = "Chase";
-console.log("my name is " + myName);
-
-favNumber = 9;
-console.log(typeof favNumber);
-
-let Joe = "Im Joe @_@ and I ";
-console.log(Joe + " like javescript");
-
-console.log("Team USA will win the world cup");
-
-let IsIronManGone = true;
-console.log(`Is Iron Man Gone : ${IsIronManGone}`);
-
-let deepQuestion = "how do you build a website?";
-console.log(deepQuestion + " You put your Boot Straps on.");
-
-let hardQuestion = "How do you write js?";
-console.log(`${hardQuestion} You react to it.`);
-
-let weirdStuff;
-console.log("Excuse me im not a null! Im " + typeof weirdStuff + ".");
-
-console.log("Java > JavaScript");
-console.log("dang im only on number 11 :(");
-
-console.log(
-  "calculating the square root of a negative number will return",
-  Math.sqrt(-10)
-);
-
-rapper = 21;
-console.log(
-  '"I dont need ID at the club cause they know that im ' +
-    rapper +
-    '" - ' +
-    rapper +
-    " Savage"
-);
-console.log("dang im only on number 13");
-
-for (let i = 0; i < 16; i++) {
-  console.log("yezir well heres number " + (i + 14));
+if (userAge > 21) {
+  console.log("You can enter the club");
+} else {
+  console.log("go home");
 }
 
-console.log("and heres 30 we did it!");
+// const sentance = "I am new to learning JavaScript";
+// console.log(sentance.slice(0, 8));
+
+// myString = "my string";
+
+// console.log("is this working");
+
+// const myName = "Chase";
+// console.log("my name is " + myName);
+
+// favNumber = 9;
+// console.log(typeof favNumber);
+
+// let Joe = "Im Joe @_@ and I ";
+// console.log(Joe + " like javascript");
+
+// console.log("Team USA will win the world cup");
+
+// let IsIronManGone = true;
+// console.log(`Is Iron Man Gone : ${IsIronManGone}`);
+
+// let deepQuestion = "how do you build a website?";
+// console.log(deepQuestion + " You put your Boot Straps on.");
+
+// let hardQuestion = "How do you write js?";
+// console.log(`${hardQuestion} You react to it.`);
+
+// let weirdStuff;
+// console.log("Excuse me im not a null! Im " + typeof weirdStuff + ".");
+
+// console.log("Java > JavaScript");
+// console.log("dang im only on number 11 :(");
+
+// console.log(
+//   "calculating the square root of a negative number will return",
+//   Math.sqrt(-10)
+// );
+
+// rapper = 21;
+// console.log(
+//   '"I dont need ID at the club cause they know that im ' +
+//     rapper +
+//     '" - ' +
+//     rapper +
+//     " Savage"
+// );
+// console.log("dang im only on number 13");
+
+// for (let i = 0; i < 16; i++) {
+//   console.log("yezir well heres number " + (i + 14));
+// }
+
+// console.log("and heres 30 we did it!");
